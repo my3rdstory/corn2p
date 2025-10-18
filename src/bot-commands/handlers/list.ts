b1968a9c02a953dfbd053fbd2b26bec01a59713b
toshi <toshi00100001@gmail.com>
2025-10-18 23:10:26 +0900

    enable list, pricelist command in telegram group

--- src/bot-commands/handlers/list.ts
+++ src/bot-commands/handlers/list.ts
@@ -1,15 +1,9 @@
 import { getListMessage } from '../../biz'
-import { NOT_ALLOWED_GROUP } from '../../biz/constants'
 import { sendMsg } from '../../biz/get-tele-bot'
 
 export default function list(bot) {
   return async (msg, match) => {
     const chatId = msg.chat.id
-
-    if (chatId < 0) {
-      sendMsg(chatId, NOT_ALLOWED_GROUP)
-      return
-    }
 
     const message = await getListMessage()
 
